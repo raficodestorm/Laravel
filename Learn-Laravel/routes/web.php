7393743d2e3e15e77dd5913b41d2349e3cc7cbe7
s-a-rafi <sarafi3258@gmail.com>
2025-10-06 08:18:45 +0600

    up

--- Learn-Laravel/routes/web.php
+++ Learn-Laravel/routes/web.php
@@ -6,10 +6,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacting', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contacting');
 
 Route::get('/abouting', function () {
     return view('about');
+});
+
+Route::get('/test/{id?}', function (string $id) {
+    if ($id) {
+        return "<h1>Your ID : " . $id . "</h1>";
+    } else {
+        return "<h1>No ID found</h1>";
+    }
 });
